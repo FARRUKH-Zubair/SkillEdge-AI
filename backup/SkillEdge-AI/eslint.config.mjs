d68e9c0c3574cc6e407f0bfae2fa863d0955e5c2
FARRUKH-Zubair <farrukhsatti406@gmail.com>
2025-05-24 23:50:23 +0500

    updated

--- backup/SkillEdge-AI/eslint.config.mjs
+++ backup/SkillEdge-AI/eslint.config.mjs
@@ -1,8 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import nextPlugin from '@next/eslint-plugin-next';
-import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,9 +13,6 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      '@next/next': nextPlugin,
-    },
     rules: {
       '@next/next/no-html-link-for-pages': 'error',
       '@next/next/no-img-element': 'error',
